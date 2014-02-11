4e6339fe451af0de55b8156d7dcd99c1caf707dd
Arnelle Balane <arnellebalane@gmail.com>
2014-02-11 23:28:42 +0800

    why up cebu dcs page initial layout and design

--- application/controllers/info.php
+++ application/controllers/info.php
@@ -4,7 +4,7 @@
 
     public function __construct() {
       parent::__construct();
-      $this->request_methods['GET'] = array('department', 'people', 'academic_programs');
+      $this->request_methods['GET'] = array('department', 'people', 'academic_programs', 'why_upcdcs');
 
       $this->_check_request_method();
       $this->load->model('meta_model', 'meta');
@@ -28,6 +28,12 @@
     public function people() {
       $data['page_title'] = 'Department Of Computer Science';
       $data['main_content'] = $this->load->view('info/people', array(), true);
+      $this->parser->parse('layouts/default', $data);
+    }
+
+    public function why_upcdcs() {
+      $data['page_title'] = 'Department Of Computer Science';
+      $data['main_content'] = $this->load->view('info/why_upcdcs', array(), true);
       $this->parser->parse('layouts/default', $data);
     }
   }
