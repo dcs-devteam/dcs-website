10929ad8d0d125ef50ac3b18cf681deef94ab9b8
Arnelle Balane <arnellebalane@gmail.com>
2014-02-24 00:20:41 +0800

    new footer layout

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,11 +1,13 @@
 <footer id="main-footer" class="clearfix">
   <div class="wrapper">
+    <a href="<?= site_url('pages/home'); ?>"><span>Department of</span>Computer Science</a>
     <nav>
       <?=anchor('/pages/home','Home')?>    
       <a href="#">Projects</a>
       <a href="#">Faculty</a>
       <a href="#">Admissions</a>
       <a href="#">About</a>
+      <p>&copy; UP Cebu DCS. Gorordo Avenue, Lahug, Cebu City, Cebu, Philippines 6000</p>
     </nav>
 
     <div class="social-networks">
