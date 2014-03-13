ed96db751d37698e2da40458a40a79e21895b802
Arnelle Balane <arnellebalane@gmail.com>
2014-03-14 01:45:12 +0800

    simplified auto-opening of info sidebar items

--- application/views/info/partials/sidebar.php
+++ application/views/info/partials/sidebar.php
@@ -1,5 +1,5 @@
 <ul class="expandable-menu">
-  <li>
+  <li class="expandable-item">
     <p>The Department</p>
     <ul>      
       <li><a href="<?= site_url('#'); ?>" data-item="message-from-the-chair">Message from the Chair</a></li>
@@ -10,7 +10,7 @@
       <li><a href="<?= site_url('#'); ?>" data-item="contact-us">Contact Us</a></li>
     </ul>
   </li>
-  <li>
+  <li class="expandable-item">
     <p>Admissions</p>
     <ul>
       <li><a href="<?= site_url('#'); ?>" data-item="bs-computer-science">BS Computer Science</a></li>
@@ -18,7 +18,7 @@
       <li><a href="<?= site_url('#'); ?>" data-item="continuing-professional-education">Continuing Professional Education</a></li>
     </ul>
   </li>
-  <li>
+  <li class="expandable-item">
     <p>Research</p>
     <ul>
       <li><a href="<?= site_url('#'); ?>" data-item="bioinformatics">Bioinformatics Research Interest Group</a></li>
@@ -29,7 +29,7 @@
       <li><a href="<?= site_url('#'); ?>" data-item="algorithms-and-complexity">Algorithms and Complexity Reseach Group</a></li>
     </ul>
   </li>
-  <li>
+  <li class="expandable-item">
     <p>People</p>
     <ul>
       <li><a href="<?= site_url('#'); ?>" data-item="faculty">Faculty</a></li>
@@ -37,7 +37,7 @@
       <li><a href="<?= site_url('#'); ?>" data-item="students">Students</a></li>
     </ul>
   </li>
-  <li>
+  <li class="expandable-item">
     <p>Life@DCS</p>
     <ul>
       <li><a href="<?= site_url('#'); ?>" data-item="news-and-events">News and Events</a></li>
@@ -47,7 +47,7 @@
       <li><a href="<?= site_url('#'); ?>" data-item="campus-activities">Campus Activities</a></li>
     </ul>
   </li>
-  <li>
+  <li class="expandable-item">
     <p>Initiatives and Society</p>
     <ul>
       <li><a href="<?= site_url('#'); ?>" data-item="summer-code-camp">Summer Code Camp</a></li>
@@ -55,7 +55,7 @@
       <li><a href="<?= site_url('#'); ?>" data-item="innovation-forum">Innovation Forum</a></li>
     </ul>
   </li>
-  <li>
+  <li class="expandable-item">
     <p>Partners</p>
     <ul>
       <li><a href="<?= site_url('#'); ?>" data-item="industry">Industry</a></li>
