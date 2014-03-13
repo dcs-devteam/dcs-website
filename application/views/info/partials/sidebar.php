550b02a90c3899adb3be04288c78c8d3a8938d87
Arnelle Balane <arnellebalane@gmail.com>
2014-03-14 01:20:45 +0800

    updated sidebar link labels in info page

--- application/views/info/partials/sidebar.php
+++ application/views/info/partials/sidebar.php
@@ -2,63 +2,66 @@
   <li>
     <p>The Department</p>
     <ul>      
-      <li><?=anchor('/info/department#vision','Vision', array('data-item'=>"td-vission"))?></li>
-      <li><?=anchor('/info/department#mission','Mission', array('data-item'=>"td-mission"))?></li>
-      <li><?=anchor('/info/department#history','History', array('data-item'=>"td-history"))?></li>
-      <li><?=anchor('/info/department#csg','Computer Science Guild', array('data-item'=>"td-csg"))?></li>            
-      <li><?=anchor('/info/department#organizational-overview','Organizational Overview', array('data-item'=>"td-organizational-overview"))?></li>
-      <li><?=anchor('/info/department#web-team','Web Team', array('data-item'=>"td-web-team"))?></li>
-      <li><?=anchor('/info/department#job-openings','Job Openings', array('data-item'=>"td-job-openings"))?></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="message-from-the-chair">Message from the Chair</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="history">History</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="vision-and-mission">Vision and Mission</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="organizational-overview">Organizational Overview</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="job-openings">Job Openings</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="contact-us">Contact Us</a></li>
     </ul>
   </li>
   <li>
-    <p>Academic Programs</p>
+    <p>Admissions</p>
     <ul>
-      <li><?=anchor('/info/academic_programs/ap-bachelor-of-science', 'Bachelor of Science', array('data-item'=>'ap-bachelor-of-science'))?></li>
-      <li><?=anchor('/info/academic_programs/ap-master-of-science', 'Master of Science', array('data-item'=>'ap-master-of-science'))?></li>
-      <li><?=anchor('/info/academic_programs/ap-continuing-education-program', 'Continuing Education Program', array('data-item'=>'ap-continuing-education-program'))?></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="bs-computer-science">BS Computer Science</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="ms-computer-science">MS Computer Science</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="continuing-professional-education">Continuing Professional Education</a></li>
     </ul>
   </li>
   <li>
-    <p>Why UP Cebu DCS?</p>
+    <p>Research</p>
     <ul>
-      <li><a href="<?= site_url('info/why_upcdcs#center-of-excellence-in-it'); ?>" data-item="wupcdcs-center-of-excellence-in-it">Center of Excellence in I.T.</a></li>
-      <li><a href="<?= site_url('info/why_upcdcs#research'); ?>" data-item="wupcdcs-research">Research</a></li>
-      <li><a href="<?= site_url('info/why_upcdcs#service-to-the-society'); ?>" data-item="wupcdcs-service-to-the-society">Service to the Society</a></li>
-      <li><a href="<?= site_url('info/why_upcdcs#national-programming-rank'); ?>" data-item="wupcdcs-national-programming-rank">National Programming Rank</a></li>
-      <li><a href="<?= site_url('info/why_upcdcs#industry-partners'); ?>" data-item="wupcdcs-industry-partners">Industry Partners</a></li>
-      <li><a href="<?= site_url('info/why_upcdcs#scholarships'); ?>" data-item="wupcdcs-scholarships">Scholarships</a></li>
-    </ul>
-  </li>
-  <li><a href="<?= site_url('info/research_lab_news'); ?>" data-item="research-lab-news">Research Lab News</a></li>
-  <li>
-    <p>Student Highlights</p>
-    <ul>
-      <li><a href="<?= site_url('info/student_highlights#comsci-week'); ?>" data-item="sh-comsci-week">ComSci Week</a></li>
-      <li><a href="<?= site_url('info/student_highlights#codetabai'); ?>" data-item="sh-codetabai">CodeTaBai!</a></li>
-      <li><a href="<?= site_url('info/student_highlights#company-technical-talk'); ?>" data-item="sh-company-technical-talk">Company Technical Talk</a></li>
-      <li><a href="<?= site_url('info/student_highlights#startup-weekend'); ?>" data-item="sh-startup-weekend">Startup Weekend</a></li>
-      <li><a href="<?= site_url('info/student_highlights#programming-competition'); ?>" data-item="sh-programming-competition">Programming Competition</a></li>
-      <li><a href="<?= site_url('info/student_highlights#lecture-series'); ?>" data-item="sh-lecture-series">Lecture Series</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="bioinformatics">Bioinformatics Research Interest Group</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="computer-vision">Computer Vision Research Group</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="software-engineering">Software Engineering Research Group</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="natural-language-processing">Natural Language Processing Research Group</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="machine-learning">Machine Learning Research Group</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="algorithms-and-complexity">Algorithms and Complexity Reseach Group</a></li>
     </ul>
   </li>
   <li>
     <p>People</p>
     <ul>
-      <li><?=anchor('/info/people#core-faculty', 'Faculty', array('data-item'=>"p-core-faculty"))?></li>
-      <li><?=anchor('/info/people#affiliate-professor', 'Affiliate Professor', array('data-item'=>"p-affiliate-professor"))?></li>
-      <li><?=anchor('/info/people#visiting-professor', 'Visiting Professor', array('data-item'=>"p-visiting-professor"))?></li>
-      <li><?=anchor('/info/people#lecturers', 'Lecturers', array('data-item'=>"p-lecturers"))?></li>
-      <li><?=anchor('/info/people#support-staff', 'Support Staff', array('data-item'=>"p-support-staff"))?></li>          
+      <li><a href="<?= site_url('#'); ?>" data-item="faculty">Faculty</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="alumni">Alumni</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="students">Students</a></li>
     </ul>
   </li>
   <li>
-    <p>Service to Society</p>
+    <p>Life@DCS</p>
     <ul>
-      <li><a href="<?= site_url('info/service_to_society#consultancy-to-lgus'); ?>" data-item="sts-consultancy-to-lgus">Consultancy to LGUs</a></li>
-      <li><a href="<?= site_url('info/service_to_society#trainings'); ?>" data-item="sts-trainings">Trainings</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="news-and-events">News and Events</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="lecture-series-and-technical-talks">Lecture Series and Technical Talks</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="featured-student-projects">Featured Student Projects</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="developer-corner">Developer Corner</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="campus-activities">Campus Activities</a></li>
     </ul>
   </li>
-  <li><a href="<?= site_url('info/alumni_features'); ?>" data-item="alumni-features">Alumni Features</a></li>
-  <li><a href="<?= site_url('info/industry_partners'); ?>" data-item="industry-partners">Industry Partners</a></li>
+  <li>
+    <p>Initiatives and Society</p>
+    <ul>
+      <li><a href="<?= site_url('#'); ?>" data-item="summer-code-camp">Summer Code Camp</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="ict-skills-enhancements-program">ICT Skills Enhancement Program</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="innovation-forum">Innovation Forum</a></li>
+    </ul>
+  </li>
+  <li>
+    <p>Partners</p>
+    <ul>
+      <li><a href="<?= site_url('#'); ?>" data-item="industry">Industry</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="government-agencies">Government Agencies</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="ngo">NGO</a></li>
+      <li><a href="<?= site_url('#'); ?>" data-item="universities">Universities</a></li>
+    </ul>
+  </li>
 </ul>
