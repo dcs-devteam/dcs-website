c340119ba11d76e8e28775e2e2c96a19677be9ea
Arnelle Balane <arnellebalane@gmail.com>
2014-03-14 09:35:11 +0800

    research page initial content

--- application/views/info/research.php
+++ application/views/info/research.php
@@ -2,54 +2,30 @@
 
 <section class="clearfix" id="bioinformatics">
   <h2>Bioinformatics Research Interest Group</h2>
-  <figure>
-    <img src="<?= base_url() . 'assets/images/sample.jpg'; ?>">
-    <figcaption>Lorem ipsum Officia magna exercitation sunt anim labore reprehenderit minim deserunt fugiat sit deserunt proident eiusmod veniam ad amet aute aliqua ea.</figcaption>
-  </figure>
-  <p>Lorem ipsum Aliquip elit est dolore et deserunt esse laborum reprehenderit do ut Ut enim id eu cupidatat irure do ea sed Excepteur aute nostrud cillum amet nulla ad non proident magna ut in culpa fugiat nulla eu nisi dolor dolore reprehenderit nulla magna sint cupidatat commodo ut sed cillum adipisicing irure irure quis exercitation mollit Excepteur consectetur Ut aliqua eu ea nisi qui pariatur sed adipisicing do veniam non laborum cupidatat laboris pariatur irure tempor quis adipisicing officia nisi consectetur aliqua ad sed dolore deserunt officia quis pariatur tempor laborum in minim fugiat deserunt pariatur Ut dolor laborum consequat laboris irure nulla Duis voluptate veniam cupidatat pariatur dolor ullamco veniam enim dolor Excepteur sint est enim aliqua occaecat minim occaecat sed in aliqua amet sint exercitation Ut in ullamco cupidatat ad laborum dolore Excepteur consectetur ut cupidatat Excepteur cupidatat non ut dolore cupidatat veniam magna minim ex reprehenderit deserunt aute sint sunt nisi cillum nostrud mollit magna ea amet eiusmod consectetur.</p>
+  <p>Interested in appylying statistical techniques in protein sequence analyses and evaluation of prediction methods. Also, interested in pursuing development of eLearning systems for complex molecular biology processes through Interactive Learning Object (ILO) or digital game-based learning (DGBL) approaches.</p>
 </section>
 
 <section class="clearfix" id="computer-vision">
   <h2>Computer Vision Research Group</h2>
-  <figure>
-    <img src="<?= base_url() . 'assets/images/sample.jpg'; ?>">
-    <figcaption>Lorem ipsum Officia magna exercitation sunt anim labore reprehenderit minim deserunt fugiat sit deserunt proident eiusmod veniam ad amet aute aliqua ea.</figcaption>
-  </figure>
-  <p>Lorem ipsum Aliquip elit est dolore et deserunt esse laborum reprehenderit do ut Ut enim id eu cupidatat irure do ea sed Excepteur aute nostrud cillum amet nulla ad non proident magna ut in culpa fugiat nulla eu nisi dolor dolore reprehenderit nulla magna sint cupidatat commodo ut sed cillum adipisicing irure irure quis exercitation mollit Excepteur consectetur Ut aliqua eu ea nisi qui pariatur sed adipisicing do veniam non laborum cupidatat laboris pariatur irure tempor quis adipisicing officia nisi consectetur aliqua ad sed dolore deserunt officia quis pariatur tempor laborum in minim fugiat deserunt pariatur Ut dolor laborum consequat laboris irure nulla Duis voluptate veniam cupidatat pariatur dolor ullamco veniam enim dolor Excepteur sint est enim aliqua occaecat minim occaecat sed in aliqua amet sint exercitation Ut in ullamco cupidatat ad laborum dolore Excepteur consectetur ut cupidatat Excepteur cupidatat non ut dolore cupidatat veniam magna minim ex reprehenderit deserunt aute sint sunt nisi cillum nostrud mollit magna ea amet eiusmod consectetur.</p>
+  <p class="upcoming-content">Content to be provided soon.</p>
 </section>
 
 <section class="clearfix" id="software-engineering">
   <h2>Software Engineering Research Group</h2>
-  <figure>
-    <img src="<?= base_url() . 'assets/images/sample.jpg'; ?>">
-    <figcaption>Lorem ipsum Officia magna exercitation sunt anim labore reprehenderit minim deserunt fugiat sit deserunt proident eiusmod veniam ad amet aute aliqua ea.</figcaption>
-  </figure>
-  <p>Lorem ipsum Aliquip elit est dolore et deserunt esse laborum reprehenderit do ut Ut enim id eu cupidatat irure do ea sed Excepteur aute nostrud cillum amet nulla ad non proident magna ut in culpa fugiat nulla eu nisi dolor dolore reprehenderit nulla magna sint cupidatat commodo ut sed cillum adipisicing irure irure quis exercitation mollit Excepteur consectetur Ut aliqua eu ea nisi qui pariatur sed adipisicing do veniam non laborum cupidatat laboris pariatur irure tempor quis adipisicing officia nisi consectetur aliqua ad sed dolore deserunt officia quis pariatur tempor laborum in minim fugiat deserunt pariatur Ut dolor laborum consequat laboris irure nulla Duis voluptate veniam cupidatat pariatur dolor ullamco veniam enim dolor Excepteur sint est enim aliqua occaecat minim occaecat sed in aliqua amet sint exercitation Ut in ullamco cupidatat ad laborum dolore Excepteur consectetur ut cupidatat Excepteur cupidatat non ut dolore cupidatat veniam magna minim ex reprehenderit deserunt aute sint sunt nisi cillum nostrud mollit magna ea amet eiusmod consectetur.</p>
+  <p class="upcoming-content">Content to be provided soon.</p>
 </section>
 
 <section class="clearfix" id="natural-language-processing">
   <h2>Natural Language Processing Research Group</h2>
-  <figure>
-    <img src="<?= base_url() . 'assets/images/sample.jpg'; ?>">
-    <figcaption>Lorem ipsum Officia magna exercitation sunt anim labore reprehenderit minim deserunt fugiat sit deserunt proident eiusmod veniam ad amet aute aliqua ea.</figcaption>
-  </figure>
-  <p>Lorem ipsum Aliquip elit est dolore et deserunt esse laborum reprehenderit do ut Ut enim id eu cupidatat irure do ea sed Excepteur aute nostrud cillum amet nulla ad non proident magna ut in culpa fugiat nulla eu nisi dolor dolore reprehenderit nulla magna sint cupidatat commodo ut sed cillum adipisicing irure irure quis exercitation mollit Excepteur consectetur Ut aliqua eu ea nisi qui pariatur sed adipisicing do veniam non laborum cupidatat laboris pariatur irure tempor quis adipisicing officia nisi consectetur aliqua ad sed dolore deserunt officia quis pariatur tempor laborum in minim fugiat deserunt pariatur Ut dolor laborum consequat laboris irure nulla Duis voluptate veniam cupidatat pariatur dolor ullamco veniam enim dolor Excepteur sint est enim aliqua occaecat minim occaecat sed in aliqua amet sint exercitation Ut in ullamco cupidatat ad laborum dolore Excepteur consectetur ut cupidatat Excepteur cupidatat non ut dolore cupidatat veniam magna minim ex reprehenderit deserunt aute sint sunt nisi cillum nostrud mollit magna ea amet eiusmod consectetur.</p>
+  <p class="upcoming-content">Content to be provided soon.</p>
 </section>
 
 <section class="clearfix" id="machine-learning">
   <h2>Machine Learning Research Group</h2>
-  <figure>
-    <img src="<?= base_url() . 'assets/images/sample.jpg'; ?>">
-    <figcaption>Lorem ipsum Officia magna exercitation sunt anim labore reprehenderit minim deserunt fugiat sit deserunt proident eiusmod veniam ad amet aute aliqua ea.</figcaption>
-  </figure>
-  <p>Lorem ipsum Aliquip elit est dolore et deserunt esse laborum reprehenderit do ut Ut enim id eu cupidatat irure do ea sed Excepteur aute nostrud cillum amet nulla ad non proident magna ut in culpa fugiat nulla eu nisi dolor dolore reprehenderit nulla magna sint cupidatat commodo ut sed cillum adipisicing irure irure quis exercitation mollit Excepteur consectetur Ut aliqua eu ea nisi qui pariatur sed adipisicing do veniam non laborum cupidatat laboris pariatur irure tempor quis adipisicing officia nisi consectetur aliqua ad sed dolore deserunt officia quis pariatur tempor laborum in minim fugiat deserunt pariatur Ut dolor laborum consequat laboris irure nulla Duis voluptate veniam cupidatat pariatur dolor ullamco veniam enim dolor Excepteur sint est enim aliqua occaecat minim occaecat sed in aliqua amet sint exercitation Ut in ullamco cupidatat ad laborum dolore Excepteur consectetur ut cupidatat Excepteur cupidatat non ut dolore cupidatat veniam magna minim ex reprehenderit deserunt aute sint sunt nisi cillum nostrud mollit magna ea amet eiusmod consectetur.</p>
+  <p>This research group focuses on the studying the principles and techniques in making machines learn and improve with data and experience. The algorithms that are studied involve statistical, computational methods and evolutionary methods. Domain investigate involves speech, social networks, and big data.</p>
 </section>
 
 <section class="clearfix" id="algorithms-and-complexity">
   <h2>Algorithms and Complexity Research Group</h2>
-  <figure>
-    <img src="<?= base_url() . 'assets/images/sample.jpg'; ?>">
-    <figcaption>Lorem ipsum Officia magna exercitation sunt anim labore reprehenderit minim deserunt fugiat sit deserunt proident eiusmod veniam ad amet aute aliqua ea.</figcaption>
-  </figure>
-  <p>Lorem ipsum Aliquip elit est dolore et deserunt esse laborum reprehenderit do ut Ut enim id eu cupidatat irure do ea sed Excepteur aute nostrud cillum amet nulla ad non proident magna ut in culpa fugiat nulla eu nisi dolor dolore reprehenderit nulla magna sint cupidatat commodo ut sed cillum adipisicing irure irure quis exercitation mollit Excepteur consectetur Ut aliqua eu ea nisi qui pariatur sed adipisicing do veniam non laborum cupidatat laboris pariatur irure tempor quis adipisicing officia nisi consectetur aliqua ad sed dolore deserunt officia quis pariatur tempor laborum in minim fugiat deserunt pariatur Ut dolor laborum consequat laboris irure nulla Duis voluptate veniam cupidatat pariatur dolor ullamco veniam enim dolor Excepteur sint est enim aliqua occaecat minim occaecat sed in aliqua amet sint exercitation Ut in ullamco cupidatat ad laborum dolore Excepteur consectetur ut cupidatat Excepteur cupidatat non ut dolore cupidatat veniam magna minim ex reprehenderit deserunt aute sint sunt nisi cillum nostrud mollit magna ea amet eiusmod consectetur.</p>
+  <p>Focus is on the application of algorithms such as scheduling and optimization.</p>
 </section>
