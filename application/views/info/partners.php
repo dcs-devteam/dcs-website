94e58a8c18b09d0067271215f347ff27583908b5
Arnelle Balane <arnellebalane@gmail.com>
2014-03-14 09:48:18 +0800

    no content indicator for initiatives and society and partners pages

--- application/views/info/partners.php
+++ application/views/info/partners.php
@@ -2,6 +2,7 @@
 
 <section class="clearfix" id="industry">
   <h2>Industry Partners</h2>
+  <!--
   <div class="partner">
     <span class="logo" style="background-image: url('<?= base_url() . 'assets/images/google.png'; ?>')"></span>
     <div class="content">
@@ -9,65 +10,21 @@
       <p>Lorem ipsum Aliquip consectetur deserunt in fugiat ullamco minim do ad labore reprehenderit eiusmod.</p>
     </div>
   </div>
-  <div class="partner">
-    <span class="logo" style="background-image: url('<?= base_url() . 'assets/images/facebook.jpg'; ?>')"></span>
-    <div class="content">
-      <h3>Facebook</h3>
-      <p>Lorem ipsum Aliquip consectetur deserunt in fugiat ullamco minim do ad labore reprehenderit eiusmod.</p>
-    </div>
-  </div>
+  -->
+  <p class="upcoming-content">Content to be provided soon.</p>
 </section>
 
 <section class="clearfix" id="government-agencies">
   <h2>Government Agency Partners</h2>
-  <div class="partner">
-    <span class="logo" style="background-image: url('<?= base_url() . 'assets/images/google.png'; ?>')"></span>
-    <div class="content">
-      <h3>Google</h3>
-      <p>Lorem ipsum Aliquip consectetur deserunt in fugiat ullamco minim do ad labore reprehenderit eiusmod.</p>
-    </div>
-  </div>
-  <div class="partner">
-    <span class="logo" style="background-image: url('<?= base_url() . 'assets/images/facebook.jpg'; ?>')"></span>
-    <div class="content">
-      <h3>Facebook</h3>
-      <p>Lorem ipsum Aliquip consectetur deserunt in fugiat ullamco minim do ad labore reprehenderit eiusmod.</p>
-    </div>
-  </div>
+  <p class="upcoming-content">Content to be provided soon.</p>
 </section>
 
 <section class="clearfix" id="ngo">
   <h2>NGO Partners</h2>
-  <div class="partner">
-    <span class="logo" style="background-image: url('<?= base_url() . 'assets/images/google.png'; ?>')"></span>
-    <div class="content">
-      <h3>Google</h3>
-      <p>Lorem ipsum Aliquip consectetur deserunt in fugiat ullamco minim do ad labore reprehenderit eiusmod.</p>
-    </div>
-  </div>
-  <div class="partner">
-    <span class="logo" style="background-image: url('<?= base_url() . 'assets/images/facebook.jpg'; ?>')"></span>
-    <div class="content">
-      <h3>Facebook</h3>
-      <p>Lorem ipsum Aliquip consectetur deserunt in fugiat ullamco minim do ad labore reprehenderit eiusmod.</p>
-    </div>
-  </div>
+  <p class="upcoming-content">Content to be provided soon.</p>
 </section>
 
 <section class="clearfix" id="universities">
   <h2>University Partners</h2>
-  <div class="partner">
-    <span class="logo" style="background-image: url('<?= base_url() . 'assets/images/google.png'; ?>')"></span>
-    <div class="content">
-      <h3>Google</h3>
-      <p>Lorem ipsum Aliquip consectetur deserunt in fugiat ullamco minim do ad labore reprehenderit eiusmod.</p>
-    </div>
-  </div>
-  <div class="partner">
-    <span class="logo" style="background-image: url('<?= base_url() . 'assets/images/facebook.jpg'; ?>')"></span>
-    <div class="content">
-      <h3>Facebook</h3>
-      <p>Lorem ipsum Aliquip consectetur deserunt in fugiat ullamco minim do ad labore reprehenderit eiusmod.</p>
-    </div>
-  </div>
+  <p class="upcoming-content">Content to be provided soon.</p>
 </section>
