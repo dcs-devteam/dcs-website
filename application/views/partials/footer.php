2c70086101aa767be2f55d7850bca3110de96f10
Arnelle Balane <arnellebalane@gmail.com>
2014-03-19 15:10:29 +0800

    modified address appearance in footer

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,6 +1,7 @@
 <footer id="main-footer" class="clearfix">
   <div class="wrapper clearfix">
     <a href="<?= site_url('pages/home'); ?>"><span>Department of</span>Computer Science</a>
+    <!--
     <nav>
       <a href="<?= site_url('pages/home'); ?>">Home</a>
       <a href="<?= site_url('info/department'); ?>">The Department</a>
@@ -8,8 +9,9 @@
       <a href="<?= site_url('info/faculty#faculty'); ?>">People</a>
       <a href="<?= site_url('info/campus_activities#campus-activities'); ?>">Life@DCS</a>
       <a href="<?= site_url('info/partners'); ?>">Partners</a>
-      <p>&copy; UP Cebu DCS. Gorordo Avenue, Lahug, Cebu City, Cebu, Philippines 6000</p>
     </nav>
+    -->
+    <small>&copy; University of the Philippines Cebu DCS<br>Gorordo Avenue, Lahug, Cebu City, Cebu, Philippines 6000</small>
 
     <div class="social-networks">
       <a href="https://www.facebook.com/upcebudcs" target="_blank" class="facebook">Facebook</a>
