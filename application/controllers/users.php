bf33723e7294fac3772fbbd2957276f5d613c49a
root <root@bt.foo.org>
2014-03-19 14:31:36 +0800

    backend stuffs

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -8,11 +8,13 @@
       
       $this->_check_request_method();
       $this->load->helper('application_helper');
+      $this->load->model('user_information_model', 'ui_model');
     }
 
     public function profile() {
       $data['page_title'] = 'Department of Computer Science';
-      $data['main_content'] = $this->load->view("users/profile", array(), true);
+      $info =  $this->ui_model->fetchUserInformation('1');
+      $data['main_content'] = $this->load->view("users/profile", array('info'=>$info), true);
       $this->parser->parse('layouts/profile', $data);
     }
 
