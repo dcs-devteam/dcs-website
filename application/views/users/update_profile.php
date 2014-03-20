5abe5bd5dbbc3e6432d32abcb5c2c23a0c8bb28b
Emmanuel Lodovice <name3anad@gmail.com>
2014-03-19 17:24:29 -0700

    edit style update user

--- application/views/users/update_profile.php
+++ application/views/users/update_profile.php
@@ -3,62 +3,59 @@
     <?=form_open("users/edit_data","POST");?>
       <h1>PERSONAL INFORMATION</h1>
       <div class="field">    
-        <div class="title"><label>First Name</label></div>
-        <div class="value"><input type="text" name="firstname" placeholder="First name" value="Emmanuel"></div>
+        <label>First Name</label>
+        <input type="text" name="firstname" placeholder="First name" value="Emmanuel">
       </div>
       <div class="field">    
-        <div class="title"><label>Last Name</label></div>
-        <div class="value"><input type="text" name="lastname" placeholder="Last name" value="Lodovice"></div>
+        <label>Last Name</label>
+        <input type="text" name="lastname" placeholder="Last name" value="Lodovice">
       </div>      
       <div class="field">    
-        <div class="title"><label>Address</label></div>
-        <div class="value"><input type="text" name="address" placeholder="Address" value="Jampang, Argao, Cebu"></div>
+        <label>Address</label>
+        <input type="text" name="address" placeholder="Address" value="Jampang, Argao, Cebu">
       </div>
       <div class="field">    
-        <div class="title"><label>Birthday</label></div>
-        <div class="value"><input type="date" name="birthday" value"2013-09-24 00:50:45"></div>
+        <label>Birthday</label>
+        <input type="date" name="birthday" value"2013-09-24 00:50:45">
       </div>
       <div class="field">    
-        <div class="title"><label>Age</label></div>
-        <div class="value"><input type="number" name="age" min="0" max="100" placeholder="Age" value="18"></div>
+        <label>Age</label>
+        <input type="number" name="age" min="0" max="100" placeholder="Age" value="18">
       </div>
       <div class="field">    
-        <div class="title"><label>Student Number</label></div>
-        <div class="value"><input type="text" name="student_number" placeholder="xxxx-xxxxx" value="2011-37567"></div>
+        <label>Student Number</label>
+        <input type="text" name="student_number" placeholder="xxxx-xxxxx" value="2011-37567">
       </div>
       <div class="field">    
-        <div class="title"><label>Course</label></div>
-        <div class="value">
-          <select name="course" placeholder="Year">
-            <option value="1">BS Computer Science</option>
-            <option value="2">MS Computer Science</option>
-          </select>
-        </div>
+        <label>Course</label>        
+        <select name="course" placeholder="Year">
+          <option value="1">BS Computer Science</option>
+          <option value="2">MS Computer Science</option>
+        </select>
       </div>
       <div class="field">    
-        <div class="title"><label>Year</label></div>
-        <div class="value"><input type="number" name="year" min="1" max="6" placeholder="Year" value="3"></div>
+        <label>Year</label>
+        <input type="number" name="year" min="1" max="6" placeholder="Year" value="3">
       </div><br/><br/>
       <h1>CONTACT DETAILS</h1>
       <div class="field">    
-        <div class="title"><label>Email</label></div>
-        <div class="value"><input type="email" name="email" placeholder="Email address" value="name3anad@gmail.com"></div>
+        <label>Email</label>
+        <input type="email" name="email" placeholder="Email address" value="name3anad@gmail.com">
       </div>
       <div class="field">    
-        <div class="title"><label>Contact #</label></div>
-        <div class="value"><input type="text" name="number" placeholder="Cellphone number" value="09229365294"></div>
+        <label>Contact #</label>
+        <input type="text" name="number" placeholder="Cellphone number" value="09229365294">
       </div>
       <div class="field">    
-        <div class="title"><label>Facebook</label></div>
-        <div class="value"><input type="url" name="facebook" placeholder="Link to your facebook page" value="http://www.facebook.com/nameANAD"></div>
+        <label>Facebook</label>
+        <input type="url" name="facebook" placeholder="Link to your facebook page" value="http://www.facebook.com/nameANAD">
       </div>
       <div class="field">    
-        <div class="title"><label>Twitter</label></div>
-        <div class="value"><input type="url" name="twiiter" placeholder="Link to your twitter page" value="http://www.twitter.com/nameanad"></div>
+        <label>Twitter</label>
+        <input type="url" name="twiiter" placeholder="Link to your twitter page" value="http://www.twitter.com/nameanad">
       </div><br/>
-      <div class="field">    
-        <div class="title"></div>
-        <div class="value"><input type="submit" value="UPDATE" class="button green"></div>
+      <div class="field">            
+        <input type="submit" value="UPDATE" class="button green">
       </div>      
     <?= form_close(); ?>
   </section>
