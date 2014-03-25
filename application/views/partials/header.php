c2820e1ec0878a62b359e524a38dcec7e1a7ae52
Arnelle Balane <arnellebalane@gmail.com>
2014-03-25 23:25:40 +0800

    current user profile section in header

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -1,6 +1,6 @@
 <div id="main-header">
   <div class="wrapper clearfix">
-    <a href="<?= site_url('pages/home'); ?>"><span>Department of</span>Computer Science</a>
+    <a href="<?= site_url('pages/home'); ?>" id="logo"><span>Department of</span>Computer Science</a>
     <!--
     <nav>
       <a href="<?= site_url('info/department'); ?>" class="<?= current_header_item($controller, $action, 'info'); ?>">Info</a>
@@ -11,5 +11,13 @@
       <a href="#" class="<?= current_header_item($controller, $action, 'contact'); ?>">Contact Us</a>
     </nav>
     -->
+
+    <a id="current-user">
+      <div class="image" style="background-image: url('<?= base_url() . 'assets/images/profile-picture.jpg'; ?>')"></div>
+      <div class="content">
+        <h2>Arnelle Balane</h2>
+        <p>Student</p>
+      </div>
+    </a>
   </div>
 </div>
