26391192a99f91915bc747fb62d52f01b3a73541
Arnelle Balane <arnellebalane@gmail.com>
2014-03-27 22:27:34 +0800

    search and filter forms in admin users listing

--- application/views/admin/users/index.php
+++ application/views/admin/users/index.php
@@ -1,5 +1,22 @@
 <div class="admin users index">
-  <h1>Users Listing</h1>
+  <header class="clearfix">
+    <h1>Users Listing</h1>
+    <form action="#" method="GET" class="filter">
+      <select name="filter">
+        <option value="1">Everyone</option>
+        <option value="1">1st Years</option>
+        <option value="1">2nd Years</option>
+        <option value="1">3rd Years</option>
+        <option value="1">4th Years</option>
+        <option value="1">Faculty</option>
+      </select>
+      <input type="submit" value="Filter" class="button green">
+    </form>
+    <form action="#" method="GET" class="search">
+      <input type="text" name="search" placeholder="search user" required>
+      <input type="submit" value="Search" class="button green">
+    </form>
+  </header>
 
   <div class="listing">
     <?php for ($i = 0; $i < 10; $i++): ?>
