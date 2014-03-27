5e2a013894dfdf8a31fb5ca6bc6b09f8c961a71e
Arnelle Balane <arnellebalane@gmail.com>
2014-03-27 21:48:10 +0800

    admin listing of all users with their designation and privileges

---
+++ application/controllers/admin.php
@@ -0,0 +1,22 @@
+<?php
+
+  class Admin extends DCS_Controller {
+
+    public function __construct() {
+      parent::__construct();
+      $this->request_methods['GET'] = array('users');
+
+      $this->_check_request_method();
+      $this->load->helper('application_helper');
+    }
+
+    public function users($page = 'index') {
+      $data['page_title'] = 'Department Of Computer Science';
+      $data['sidebar_content'] = $this->load->view('partials/sidebar', array(), true);
+      $data['main_content'] = $this->load->view('admin/users/' . $page, array(), true);
+      $this->parser->parse('layouts/default', $data);
+    }
+
+  }
+
+?>
---
+++ application/views/admin/users/index.php
@@ -0,0 +1,17 @@
+<div class="admin users index">
+  <h1>Users Listing</h1>
+
+  <div class="listing">
+    <?php for ($i = 0; $i < 10; $i++): ?>
+    <a href="#">
+      Arnelle Balane
+      <small>Student</small>
+      <div class="privileges">
+        <span class="create-news" title="create news articles"></span>
+        <span class="create-news" title="create news articles"></span>
+        <span class="create-news" title="create news articles"></span>
+      </div>
+    </a>
+  <?php endfor; ?>
+  </div>
+</div>
