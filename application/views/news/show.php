3618107c411c8cfe3781f78a76ef47a008392d54
Arnelle Balane <arnellebalane@gmail.com>
2014-03-28 00:20:03 +0800

    added news author and date info in show news page

--- application/views/news/show.php
+++ application/views/news/show.php
@@ -1,10 +1,14 @@
 <?php if ($news) : ?>
   <div class="news">
-    <h1><?=$news->title?></h1>
+    <header>
+      <h1><?= $news->title; ?></h1>
+      <h2>Arnelle Balane</h2>
+      <time>March 27, 2014</time>
+    </header>
     <?php if ($news->name != NULL) : ?>
       <img src="<?= base_url() . $news->name; ?>" />
     <?php endif; ?>
-    <?=$news->content?>
+    <?= $news->content; ?>
   </div>
 <?php else : ?>  
 <?php endif; ?>
