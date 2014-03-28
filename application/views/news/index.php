5f8cddf6fc1d5054db9d06c50f4edc22f1a84276
Arnelle Balane <arnellebalane@gmail.com>
2014-03-28 21:12:38 +0800

    default news photo

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -4,7 +4,8 @@
     <a href="<?= site_url('news/make'); ?>" class="button green">Add News Article</a>
   </header>
   <?php foreach ($news as $item) : ?>
-    <a href="<?= site_url('news/show/'.$item->slug.'#news-and-events'); ?>" class="news" style="background-image: url('<?= ($item->name != NULL) ? base_url() . $item->name : '';?>');">
+    <?php $background = ($item->name != NULL) ? base_url() . $item->name : base_url() . 'assets/images/news-images/default.png'; ?>
+    <a href="<?= site_url('news/show/'.$item->slug.'#news-and-events'); ?>" class="news" style="background-image: url('<?= $background; ?>');">
       <div class="content">
         <p><?=$item->title?></p>
         <time><?=strftime('%B %d, %Y', strtotime($item->date))?></time>
@@ -13,6 +14,6 @@
   <?php endforeach; ?>
 
   <?php if (empty($news)): ?>
-    <h2>There are currently no news articles.</h2>
+    <h2 class="empty-thread">There are currently no news articles.</h2>
   <?php endif; ?>
 </section>
