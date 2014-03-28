5c3ec488b8fbde137a1b935b54d0b51b5f824041
aldnav <aldrinnavarro16@gmail.com>
2014-03-28 22:35:26 +0800

    updated faculty view, department view, cockpit model, info controller

--- application/views/info/faculty.php
+++ application/views/info/faculty.php
@@ -3,6 +3,10 @@
     <h1>Faculty</h1>
     
     <div class="tile-wrapper clearfix">
+      <?php foreach ($faculty as $teacher): ?>
+        <?php echo $teacher->body; ?>
+      <?php endforeach; ?>
+      
       <div class="tile clearfix">
         <div class="image" style="background-image: url('<?= base_url() . 'assets/images/sample-project.jpg' ?>');"></div>
         <div class="information-container clearfix">
@@ -26,102 +30,7 @@
           </div>
         </div>
       </div>    
-
-      <div class="tile clearfix">
-        <div class="image" style="background-image: url('<?= base_url() . 'assets/images/sample-project.jpg' ?>');"></div>
-        <div class="information-container clearfix">
-          <div class="field">          
-            <p>Emmanuel T. Lodovice</p>
-          </div>
-          <div class="field">          
-            <p>Faculty 1</p>
-          </div>
-          <div class="field">          
-            <p>name3anad@gmail.com</p>
-          </div>
-          <div class="field">          
-            <ul>
-              <li>Masters in Computer Science at The University of the Philippines </li>
-              <li>Masters in Engineering at MIT</li>
-            </ul>
-          </div>
-          <div class="field">              
-            <p>AI, Modeling and Simulation</p>
-          </div>
-        </div>
-      </div>    
-
-      <div class="tile clearfix">
-        <div class="image" style="background-image: url('<?= base_url() . 'assets/images/sample-project.jpg' ?>');"></div>
-        <div class="information-container clearfix">
-          <div class="field">          
-            <p>Emmanuel T. Lodovice</p>
-          </div>
-          <div class="field">          
-            <p>Faculty 1</p>
-          </div>
-          <div class="field">          
-            <p>name3anad@gmail.com</p>
-          </div>
-          <div class="field">          
-            <ul>
-              <li>Masters in Computer Science at The University of the Philippines </li>
-              <li>Masters in Engineering at MIT</li>
-            </ul>
-          </div>
-          <div class="field">              
-            <p>AI, Modeling and Simulation</p>
-          </div>
-        </div>
-      </div>    
-
-      <div class="tile clearfix">
-        <div class="image" style="background-image: url('<?= base_url() . 'assets/images/sample-project.jpg' ?>');"></div>
-        <div class="information-container clearfix">
-          <div class="field">          
-            <p>Emmanuel T. Lodovice</p>
-          </div>
-          <div class="field">          
-            <p>Faculty 1</p>
-          </div>
-          <div class="field">          
-            <p>name3anad@gmail.com</p>
-          </div>
-          <div class="field">          
-            <ul>
-              <li>Masters in Computer Science at The University of the Philippines </li>
-              <li>Masters in Engineering at MIT</li>
-            </ul>
-          </div>
-          <div class="field">              
-            <p>AI, Modeling and Simulation</p>
-          </div>
-        </div>
-      </div>    
-
-      <div class="tile clearfix">
-        <div class="image" style="background-image: url('<?= base_url() . 'assets/images/sample-project.jpg' ?>');"></div>
-        <div class="information-container clearfix">
-          <div class="field">          
-            <p>Emmanuel T. Lodovice</p>
-          </div>
-          <div class="field">          
-            <p>Faculty 1</p>
-          </div>
-          <div class="field">          
-            <p>name3anad@gmail.com</p>
-          </div>
-          <div class="field">          
-            <ul>
-              <li>Masters in Computer Science at The University of the Philippines </li>
-              <li>Masters in Engineering at MIT</li>
-            </ul>
-          </div>
-          <div class="field">              
-            <p>AI, Modeling and Simulation</p>
-          </div>
-        </div>
-      </div>    
+      
     </div>      
     
     <br>
